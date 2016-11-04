beaed6a76c2f6a0458c5b8c34dfb2c7e4cd8e74d
Ryan Dai <daiyuan1994@hotmail.com>
2016-11-04 23:11:37 +1000

    web_service_calculator
    A smple calculator (and front-end gui)allows users to calculate and
    transform between decimal and binary.

---
+++ WebPageSeperated.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="WebPageSeperated.aspx.cs" Inherits="WebPageSeperated" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p>
+            Welcome to prac1</p>
+        <p>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Calculate" />
+&nbsp;<asp:TextBox ID="FirstNumber" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="DropDownList1" runat="server">
+                <asp:ListItem Selected="True">+</asp:ListItem>
+                <asp:ListItem>-</asp:ListItem>
+                <asp:ListItem>*</asp:ListItem>
+                <asp:ListItem>/</asp:ListItem>
+            </asp:DropDownList>
+        &nbsp;&nbsp;
+            <asp:TextBox ID="SecondNumber" runat="server"></asp:TextBox>
+&nbsp;&nbsp;
+            <asp:Label ID="Label1" runat="server" Text="=Base10:"></asp:Label>
+            <asp:TextBox ID="BaseTenResult" runat="server" ReadOnly="True"></asp:TextBox>
+&nbsp;&nbsp;
+            <asp:Label ID="Label2" runat="server" Text="=Base2:"></asp:Label>
+            <asp:TextBox ID="BaseTwoResult" runat="server" ReadOnly="True"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Button ID="Count" runat="server" OnClick="Count_Click" Text="Count" />
+        </p>
+        <p>
+            <asp:Label ID="Label3" runat="server" Text="Num of 0:"></asp:Label>
+            <asp:TextBox ID="NumOfZero" runat="server"></asp:TextBox>
+        </p>
+        <p>
+            <asp:Label ID="Label4" runat="server" Text="Num of 1:"></asp:Label>
+            <asp:TextBox ID="NumOfOne" runat="server"></asp:TextBox>
+        </p>
+    </form>
+</body>
+</html>
---
+++ WebPageSeperated.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class WebPageSeperated : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string first = FirstNumber.Text;
+        string second = SecondNumber.Text;
+        int firstNum = Convert.ToInt16(first);
+        int secondNum = Convert.ToInt16(second);
+        string operation = DropDownList1.SelectedItem.Value;
+        int result = 0;
+        if (operation.Equals("+"))
+        {
+            result = firstNum + secondNum;
+        }
+        if (operation.Equals("-"))
+        {
+            result = firstNum - secondNum;
+        }
+        if (operation.Equals("*"))
+        {
+            result = firstNum * secondNum;
+        }
+        if (operation.Equals("/"))
+        {
+            result = firstNum / secondNum;
+        }
+        BaseTenResult.Text = Convert.ToString(result);
+
+        string binary = Convert.ToString(result,2);
+        BaseTwoResult.Text = binary;
+    }
+
+    protected void Count_Click(object sender, EventArgs e)
+    {
+        string first = FirstNumber.Text;
+        string second = SecondNumber.Text;
+        int firstNum = Convert.ToInt16(first);
+        int secondNum = Convert.ToInt16(second);
+        string operation = DropDownList1.SelectedItem.Value;
+        int result = 0;
+        if (operation.Equals("+"))
+        {
+            result = firstNum + secondNum;
+        }
+        if (operation.Equals("-"))
+        {
+            result = firstNum - secondNum;
+        }
+        if (operation.Equals("*"))
+        {
+            result = firstNum * secondNum;
+        }
+        if (operation.Equals("/"))
+        {
+            result = firstNum / secondNum;
+        }
+        BaseTenResult.Text = Convert.ToString(result);
+
+        string binary = Convert.ToString(result, 2);
+
+        int countZero = 0;
+        int countOne = 0;
+        for(int i = 0; i < binary.Length; i++)
+        {
+            if (binary[i] == '0')
+            {
+                countZero++;
+            }
+            if (binary[i] == '1')
+            {
+                countOne++;
+            }
+        }
+        NumOfZero.Text = Convert.ToString(countZero);
+        NumOfOne.Text = Convert.ToString(countOne);
+    }
+}
